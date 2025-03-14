47af954dbcfc79674e43215b2c250e32b745e976
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2025-03-14 14:53:53 +0600

    Added social login

--- src/app/login/components/LoginForm.jsx
+++ src/app/login/components/LoginForm.jsx
@@ -1,9 +1,36 @@
 "use client";
+import toast from "react-hot-toast";
 import Link from "next/link";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import SocialLogin from "./SocialLogin";
 
 const LoginForm = () => {
+  const router = useRouter();
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    toast.loading("Submitting");
+    try {
+      await signIn("credentials", {
+        email,
+        password,
+        callbackUrl: "/",
+        redirect: false,
+      });
+
+      // if (response.ok) {
+      //   toast.success("Logged in successfully");
+      //   router.push("/");
+      //   form.reset();
+      // } else {
+      //   toast.error("Failed to log in");
+      // }
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <form onSubmit={handleSubmit} className="w-full max-w-lg space-y-8">
@@ -29,11 +56,11 @@ const LoginForm = () => {
           className="input input-bordered w-full"
         />
       </label>
-      <button className="w-full h-12 bg-orange-500 text-white font-bold">
+      <button className="cursor-pointer w-full h-12 bg-orange-500 text-white font-bold">
         Sign In
       </button>
       <p className="text-center">Or Sign In with</p>
-      {/* <SocialLogin /> */}
+      <SocialLogin />
       <p className="text-center">
         Already have an account?{" "}
         <Link href="/register" className="text-orange-500 font-bold">
